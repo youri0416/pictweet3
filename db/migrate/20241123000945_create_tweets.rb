7c3126021133562a3977d358e2546a14945dcb46
youri0416 <withuuuu0416@gmail.com>
2024-11-23 09:13:21 +0900

    テーブル作成

--- db/migrate/20241123000945_create_tweets.rb
+++ db/migrate/20241123000945_create_tweets.rb
@@ -1,6 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
+      t.string :name
+      t.string :text
+      t.string :image
 
       t.timestamps
     end

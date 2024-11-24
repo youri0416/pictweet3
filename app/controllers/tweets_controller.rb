2e22d0be62f9d41cae48f66a9c3405fca0959c27
youri0416 <withuuuu0416@gmail.com>
2024-11-24 09:53:49 +0900

    createアクション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,14 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end

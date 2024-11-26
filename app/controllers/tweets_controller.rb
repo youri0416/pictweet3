8a1929598e4e9768ebff641f8f98d613084ec50d
youri0416 <withuuuu0416@gmail.com>
2024-11-26 09:14:50 +0900

    コントローラー

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,12 @@ class TweetsController < ApplicationController
     redirect_to '/'
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)

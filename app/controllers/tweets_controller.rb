3fe2675fb852ed0882dfc92fd4297bd970d2e43c
youri0416 <withuuuu0416@gmail.com>
2024-12-03 08:04:44 +0900

    コントローラー作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,6 +28,10 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
